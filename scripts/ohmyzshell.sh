df2c082cd697e88428cbbd956d41917ddeb1f60e
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2020-01-21 11:54:43 -0300

    updating ohmyzshell.sh

--- scripts/ohmyzshell.sh
+++ scripts/ohmyzshell.sh
@@ -1,3 +1,9 @@
+if ! [ -x "$(command -v zsh)" ]; then
+  echo 'Error: zsh is not installed.' >&2
+  sudo apt install -y zsh > apt_zsh.log 2> apt_zsh_error.log
+fi
+
+
 if ! [ -x "$(command -v curl)" ]; then
   echo 'Error: curl is not installed.' >&2
   exit 1
